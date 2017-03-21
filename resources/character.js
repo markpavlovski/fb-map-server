d64b63db1692c7a4125bdab34df02c9d1cbbfb9f
Wes Reid <bwreid@gmail.com>
2017-03-21 09:28:44 -0700

    Add basic exercises and instructions

---
+++ resources/character.js
@@ -0,0 +1,137 @@
+module.exports = [
+  {
+    name: 'Frodo Baggins',
+    id: '040f873d-df27-4f5c-8161-25ac0357ea35',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Samwise Gamgee',
+    id: 'a60ef22f-e3e6-47c6-9171-33848a63fa50',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Meriadoc Brandybuck (Merry)',
+    id: 'a600d137-9362-4636-ad56-be45a5c19816',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Peregrin Took (Pippin or Pip)',
+    id: '2d307188-0073-4d36-8f30-74e4efc687a4',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Gandalf the Grey',
+    id: 'c8e4c548-f66e-4976-a158-7752ead05bff',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Aragorn',
+    id: 'e3adda3f-2536-4b6d-9a5a-de81f5f50129',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Legolas Greenleaf',
+    id: '23f3b017-d222-4b02-b246-8cc5cf9a559a',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Gimli',
+    id: '5e44b01b-885e-47eb-ad32-bcc22682a065',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Boromir',
+    id: '7ccada36-2de9-4ac4-944a-467a02572b6a',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Sauron',
+    id: '14fb9197-c0f0-493f-a860-02f8902327e9',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'The Witch-king of Angmar',
+    id: '5df0f463-75ee-449c-a9ab-f1829b9ccedf',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Saruman the White',
+    id: '500a7390-db23-47e4-b625-1f9416704e3d',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Gríma Wormtongue',
+    id: 'a13ca440-37d8-4c39-b03b-68135993169a',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Gollum',
+    id: '03130837-e6ff-44db-bf36-7b65633863d9',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Shelob',
+    id: '3521c55a-414e-46cf-a6f0-c3f48f46366b',
+    series_id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498'
+  },
+  {
+    name: 'Buffy Summers',
+    id: 'b21eb607-87c8-4bce-8e77-885b7f3a18b3',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Willow Rosenberg',
+    id: '07236036-8ed0-4fe5-ae4d-d88d93f16349',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Xander Harris',
+    id: '005e1b81-0285-4acf-b083-c84e53d097ac',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Rupert Giles',
+    id: '4a2a8a31-5ec6-4bab-8bfb-7dacaab18415',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Spike',
+    id: 'f9c0ec05-7dfb-4215-b753-f2508e139ca7',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Anya Jenkins',
+    id: '8b0660f8-cf0e-477d-9236-df8bfd5e9aa6',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Dawn Summers',
+    id: 'e3a65e12-7110-4442-81de-ae8579efc089',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Angel',
+    id: '6e00203e-367d-4e06-bef2-f5053a888a43',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Cordelia Chase',
+    id: '10aadf6d-5cd5-453a-9e79-9fa54665c6b4',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Tara Maclay',
+    id: '50ba118a-4a58-48ea-9ae3-c73f70ad6122',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Daniel "Oz" Osbourne',
+    id: '11121784-66a8-44ca-867d-78dbb083d3d5',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  },
+  {
+    name: 'Riley Finn',
+    id: '932c3400-516c-4019-becb-4fd9fc5cd498',
+    series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
+  }
+]
---
+++ resources/series.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    id: 'd32336ad-a734-4d32-8732-21543a18dfbc',
+    name: 'Buffy the Vampire Slayer'
+  },
+  {
+    id: 'b373072c-80df-4fae-8cfd-e8cfb07a0498',
+    name: 'The Lord of the Rings'
+  }
+]

0261b4a887a86494d494ffa5f2c1fce328dcca11
Wes Reid <bwreid@gmail.com>
2017-10-09 20:41:01 -0700

    Add more characters

--- db/characters.js
+++ db/characters.js
@@ -133,5 +133,241 @@ module.exports = [
     name: 'Riley Finn',
     id: '932c3400-516c-4019-becb-4fd9fc5cd498',
     series_id: 'd32336ad-a734-4d32-8732-21543a18dfbc'
-  }
+  },
+  {
+    name: "Tee Watt Kaa",
+    id: '2d8b0623-d604-4917-b826-678f3349b739',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Gideon Hask",
+    id: '525bc2c0-13d0-43b8-9f2e-fec315dd49a5',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Black Krrsantan",
+    id: 'c9782785-ce78-40a2-837f-054059790c84',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Feral",
+    id: '167519eb-fbde-46c7-bb13-d9cd93625b54',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Amee",
+    id: '91973e6f-5634-41c4-b4ed-8028e79a467a',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Jabba the Hutt",
+    id: 'eeff7e4d-dafc-4fd2-b9eb-9e0757fe62d2',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Cordé",
+    id: 'e93797b3-af1e-46a5-ad0e-d2e36d5d7418',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Grand Admiral Thrawn",
+    id: '7da815a4-5d68-4aed-977f-77f7a41b7f60',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Wes Janson",
+    id: '01ee262c-f5b5-4000-9a9d-28ff6e01cd6f',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Pablo-Jill",
+    id: 'dc5deb23-ad15-4f21-9261-fceaea799158',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Saelt-Marae (\"Yak Face\")",
+    id: '9d0ffa00-bc23-4fcb-ad6d-53f41b85617d',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Mawhonic",
+    id: '18a714eb-f15e-44c7-a2b8-025b4d7b25d8',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Ciena Ree",
+    id: '65ececb8-f7f9-4cdf-9113-c75c1f4fea15',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Biggs Darklighter",
+    id: '7086960e-823c-4ab3-abf8-f20a1fff85fd',
+    series_id: 'f2a4879b-7157-45ce-b570-5afe7efb6598'
+  },
+  {
+    name: "Mrs. Boswell",
+    id: 'd13efdda-ea9c-46ea-8490-2df8f68d265a',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Letty Ortiz",
+    id: 'c2146961-accc-41d5-9f96-fe4cdad79abc',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Twinkie",
+    id: 'e2a1b399-8aca-49ab-b573-d75928f85288',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Han Seoul-Oh",
+    id: '7be8679c-3d37-4b1a-a85d-c41bc8ffad07',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Carter Verone",
+    id: '94b9af60-f5d0-4d4c-a5d5-026c7c073283',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Jah",
+    id: 'fc5f6304-a455-49a5-a29a-019c8856806f',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Denlinger",
+    id: 'ff79919d-3749-4590-891f-4f0d11d739ce',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Hernan Reyes",
+    id: '27ce9672-9ac5-43c1-ba32-b13e40ee7088',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Owen Shaw",
+    id: '592d1221-463b-4a2f-8f40-1711291060fe',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Jack O'Conner",
+    id: '0247aab9-d3f6-44d0-b62e-006ad536fc3c',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Agent Wilkes",
+    id: '77c8435c-a48a-47e2-8fc5-d2dae5578cc0',
+    series_id: 'd7be703f-ae4d-4f9e-be5f-ad0fa2a64ba2'
+  },
+  {
+    name: "Shayde",
+    id: '24565f34-42ae-41ac-9805-2f3b56df4513',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Timewyrm",
+    id: 'cc183152-a299-4752-ab02-b3e75713f5d8',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Cybermats",
+    id: 'a9f11201-1542-4197-bb42-5e6be26cf3d2',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Muriel Frost",
+    id: 'dbee17dd-0829-46ae-8bd1-cc9f5dbcb547',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Cybermen",
+    id: 'cb4abc16-9831-4747-ae4a-67e876dfc41e',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Ice Warriors",
+    id: '62a2b6b0-24a4-4b2a-a3ef-f31a31bb56f3',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Sea Devils",
+    id: '0a199801-9a3f-481b-8b15-7b9966d38140',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Judoon",
+    id: 'cc1db74d-de3d-4b0f-97dc-04e11fc68071',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Sontarans",
+    id: '928e7dda-cf0f-4342-8e28-6b9ae37a12a5',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Iris Wildthyme",
+    id: 'b6282abb-5817-4930-9272-44882e57a380',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Beep the Meep",
+    id: 'dc0b9984-1728-408b-9294-687a133b15b5',
+    series_id: '8ae031bb-25fb-439b-b8c4-618fff62248f'
+  },
+  {
+    name: "Jacob Black",
+    id: '0c4673aa-a97b-4dd4-9658-a0c02567628d',
+    series_id: '0371632b-583f-47d3-bc63-d2c5becebd4c'
+  },
+  {
+    name: "Bella Swan",
+    id: 'c7b74711-247d-49e1-96be-e58f11e4d22f',
+    series_id: '0371632b-583f-47d3-bc63-d2c5becebd4c'
+  },
+  {
+    name: "Edward Cullen",
+    id: 'ddd5df10-e18a-464a-be65-24bebfb7ad83',
+    series_id: '0371632b-583f-47d3-bc63-d2c5becebd4c'
+  },
+  {
+    name: "Agent Brown",
+    id: '63d6dacf-6199-4590-9c48-94255cf0a0fd',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+  {
+    name: "The Architect",
+    id: '8701549d-13e3-45f8-93d0-5e8b8a7a1d3c',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+  {
+    name: "Captain Mifune",
+    id: '1cbb9b84-26b7-4e4d-b330-7026ac19f2de',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+  {
+    name: "Captain Roland",
+    id: '5c6453fb-4ec5-4237-b30c-07812084fa9f',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+  {
+    name: "Agent Thompson",
+    id: '01e1697f-eed9-4ba2-8d65-26786d0777fb',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+  {
+    name: "Morpheus",
+    id: '5d045d9a-d786-4c77-84b2-c95741cbc8ae',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+  {
+    name: "Agent Smith",
+    id: '7e5c75b6-4c37-4ce1-b72c-c712a4e03c64',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+  {
+    name: "Agent Jones",
+    id: 'cd19c955-402b-46a2-b044-1957ca2e544c',
+    series_id: '98f900c6-7845-4ca5-8640-a361bd7d77ed'
+  },
+
 ]

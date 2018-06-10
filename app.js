ddb8e7df9b95799869664091a6a3784129cb1da2
Mark Pavlovski <markpavlovski@gmail.com>
2018-06-10 10:28:49 -0700

    added dynamic map query

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 9999
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const uuid = require('uuid/v4')
@@ -50,8 +50,91 @@ let validateSeries =  (req, res, next) => {
   }
 }
 
-app.get('/series', (req, res) => {
-  res.send(db.series)
+app.get('/', (req, res) => {
+ const map = `<html>
+    <head></head>
+    <body>
+      <img src='http://placecage.com/300/300'/>
+    </body>
+  </html>`
+
+  console.log(req.query)
+
+  const centerLocation = req.query.latlng ? req.query.latlng : '47.628701, -122.343065'
+
+
+  const page = `
+  <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Multiple Markers Google Maps</title>
+        <style>
+          * {
+            margin: 0px;
+            padding: 0px;
+          }
+        </style>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.11&sensor=false" type="text/javascript"></script>
+        <script type="text/javascript">
+        // check DOM Ready
+        $(document).ready(function() {
+            // execute
+            (function() {
+                // map options
+                var options = {
+                    zoom: 17,
+                    center: new google.maps.LatLng(${centerLocation}), // centered US
+                    // mapTypeId: google.maps.MapTypeId.TERRAIN,
+                    mapTypeControl: false
+                };
+
+                // init map
+                var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+
+                // NY and CA sample Lat / Lng
+                //var southWest = new google.maps.LatLng(47.627080, -122.347431);
+                //var northEast = new google.maps.LatLng(47.633025, -122.341202);
+                var southWest = new google.maps.LatLng(47.624609, -122.365359);
+                var northEast = new google.maps.LatLng(47.638253, -122.340436);
+
+                var lngSpan = northEast.lng() - southWest.lng();
+                var latSpan = northEast.lat() - southWest.lat();
+
+                // set multiple marker
+                for (var i = 0; i < 25; i++) {
+                    // init markers
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(southWest.lat() + latSpan * Math.random(), southWest.lng() + lngSpan * Math.random()),
+                        map: map,
+                        title: 'Click Me ' + i
+                    });
+
+                    // process multiple info windows
+                    (function(marker, i) {
+                        // add click event
+                        google.maps.event.addListener(marker, 'click', function() {
+                            infowindow = new google.maps.InfoWindow({
+                                content: 'Hello, World!!'
+                            });
+                            infowindow.open(map, marker);
+                        });
+                    })(marker, i);
+                }
+            })();
+        });
+        </script>
+    </head>
+    <body>
+        <div id="map_canvas" style="width: 100vw; height:100vh;"></div>
+    </body>
+</html>
+
+  `
+
+  res.send(page)
 })
 
 app.get('/series/:id', validateSeries, (req, res) => {
